392d068ca7c8b2fdd3bd315af29bb7f0ef42c8d5
Vikalp Mishra <vikalpmishra783@gmail.com>
2024-10-19 22:08:22 +0530

    add sanctum, admin routes, api routes, roles, user_role

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::resource('products', ProductController::class);
+// });
