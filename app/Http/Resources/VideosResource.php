2e2e905a82db5bf68c90ff7985155f6278247138
Vikalp Mishra <vikalpmishra783@gmail.com>
2024-11-16 00:26:25 +0530

    send only video id in case of youtube videos

--- app/Http/Resources/VideosResource.php
+++ app/Http/Resources/VideosResource.php
@@ -14,11 +14,25 @@ class VideosResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if ($this->src_type == 'url' && preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $this->src, $match)) {
+            return [
+                'id' => $this->id,
+                'title' => $this->title,
+                'img' => $this->img_type === 'file' ? asset('contents/img/' . $this->img) : $this->img,
+                'src' => $match[1],
+                'url_type' => 'youtube',
+                'duration' => $this->duration,
+                'tags' => explode(',', $this->tags),
+                'creator' => $this->creator,
+                'about' => $this->about,
+            ];
+        }
         return [
             'id' => $this->id,
             'title' => $this->title,
             'img' => $this->img_type === 'file' ? asset('contents/img/' . $this->img) : $this->img,
             'src' => $this->src_type === 'file' ? asset('contents/file/' . $this->src) : $this->src,
+            'url_type' => 'other',
             'duration' => $this->duration,
             'tags' => explode(',', $this->tags),
             'creator' => $this->creator,
