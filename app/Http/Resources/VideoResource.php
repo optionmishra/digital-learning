ced9dc3cf8944bcbb53bed0cf69888b09a80289e
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-04-14 16:52:04 +0530

    remove url type

--- app/Http/Resources/VideoResource.php
+++ app/Http/Resources/VideoResource.php
@@ -14,30 +14,30 @@ class VideoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $urlType = 'other';
-        $src = $this->src;
+        // $urlType = 'other';
+        // $src = $this->src;
 
-        if ($this->src_type === 'url') {
-            // Check for YouTube video
-            if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $this->src, $match)) {
-                $src = $match[1];
-                $urlType = 'youtube';
-            }
-            // Check for Vimeo video
-            elseif (preg_match('~^https?://(?:www\.)?vimeo\.com/(?:channels/(?:\w+/)?|groups/([^/]*)/videos/|)(\d+)(?:|/\?)~', $this->src, $match)) {
-                $urlType = 'vimeo';
-            }
-        } else if ($this->src_type === 'file') {
-            $src = asset('contents/file/' . $this->src);
-        }
+        // if ($this->src_type === 'url') {
+        //     // Check for YouTube video
+        //     if (preg_match('%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i', $this->src, $match)) {
+        //         $src = $match[1];
+        //         $urlType = 'youtube';
+        //     }
+        //     // Check for Vimeo video
+        //     elseif (preg_match('~^https?://(?:www\.)?vimeo\.com/(?:channels/(?:\w+/)?|groups/([^/]*)/videos/|)(\d+)(?:|/\?)~', $this->src, $match)) {
+        //         $urlType = 'vimeo';
+        //     }
+        // } else if ($this->src_type === 'file') {
+        //     $src = asset('contents/file/' . $this->src);
+        // }
 
         return [
             'id' => $this->id,
             'title' => $this->title,
             'topic' => TopicResource::make($this->topic),
             'img' => $this->img_type === 'file' ? asset('contents/img/' . $this->img) : $this->img,
-            'src' => $src,
-            'url_type' => $urlType,
+            'src' => $this->src,
+            // 'url_type' => $urlType,
             'duration' => $this->duration,
             'tags' => explode(',', $this->tags),
             'creator' => $this->creator,
