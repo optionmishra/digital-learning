ec8c605800967027021fc48e8d8c87de579fd684
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-04-17 01:02:37 +0530

    add CI db config

--- config/database.php
+++ config/database.php
@@ -62,6 +62,27 @@ return [
             ]) : [],
         ],
 
+        // CodeIgniter database connection
+        'codeigniter' => [
+            'driver' => 'mysql',
+            'url' => env('CI_DATABASE_URL'),
+            'host' => env('CI_DB_HOST', '127.0.0.1'),
+            'port' => env('CI_DB_PORT', '3306'),
+            'database' => env('CI_DB_DATABASE', 'forge'),
+            'username' => env('CI_DB_USERNAME', 'forge'),
+            'password' => env('CI_DB_PASSWORD', ''),
+            'unix_socket' => env('CI_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -130,6 +151,11 @@ return [
         'update_date_on_publish' => true,
     ],
 
+    'ci_migration' => [
+        'source' => env('CI_DB_CONNECTION', 'codeigniter'),
+        'target' => env('LARAVEL_DB_CONNECTION', 'mysql'),
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -147,7 +173,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
