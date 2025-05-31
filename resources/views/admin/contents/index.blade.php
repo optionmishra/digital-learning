b3e7f9dc66fc26ef4e590a3d6e5544bc08e40238
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-05-31 18:24:26 +0530

    fix: base - update books on modal show event

--- resources/views/admin/contents/index.blade.php
+++ resources/views/admin/contents/index.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="body flex-grow-1">
-        <div class="px-4 container-lg">
-            <div class="mb-4 row card">
+        <div class="container-lg px-4">
+            <div class="row card mb-4">
                 <div class="card-header">
                     <div class="d-flex justify-content-between align-items-center">
                         <h5 class="card-title">{{ $contentType->name }}s</h5>
-                        <button class="px-2 py-2 btn btn-primary" type="button" title="Edit" data-coreui-toggle="modal"
+                        <button class="btn btn-primary px-2 py-2" type="button" title="Edit" data-coreui-toggle="modal"
                             data-coreui-target="#contentStore">Create</button>
                     </div>
                 </div>
                 <div class="card-body table-responsive">
-                    <table class="table table-striped table-bordered"
+                    <table class="table-striped table-bordered table"
                         data-table-route="{{ route('admin.contents.datatable') }}">
                         <thead>
                             <tr>
@@ -45,6 +45,7 @@
         const topics = @json($topics);
 
         function updateBooks() {
+            console.log('update books');
             const standard = document.getElementById("standard").value;
             const subject = document.getElementById("subject").value;
             const bookSelect = document.getElementById("book");
@@ -80,6 +81,14 @@
                     }
                 });
             }
+        }
+
+        const modalElement = document.querySelector(".modal");
+
+        if (modalElement) {
+            modalElement.addEventListener("show.coreui.modal", function() {
+                updateBooks();
+            });
         }
     </script>
 @endsection
