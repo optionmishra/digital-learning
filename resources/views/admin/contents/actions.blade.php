b15ccda01421553a12b846961e6d02a6af9a36db
Vikalp Mishra <vikalpmishra783@gmail.com>
2025-08-25 19:00:20 +0530

    feat: Add duration to content edit modal data

--- resources/views/admin/contents/actions.blade.php
+++ resources/views/admin/contents/actions.blade.php
@@ -2,7 +2,7 @@
     <button class="btn btn-link nav-link d-flex align-items-center edit-btn px-2 py-2" type="button" title="Edit"
         data-coreui-toggle="modal" data-coreui-target="#contentStore"
         data-update-route="{{ route('admin.contents.store') }}"
-        data-row-data="{{ json_encode(['id' => $content->id, 'title' => $content->title, 'tags' => $content->tags, 'img_type' => $content->img_type, 'img_url' => $content->img, 'src_type' => $content->src_type, 'url' => $content->src, 'price' => $content->price, 'about' => $content->about, 'standard_id' => $content->standard_id, 'subject_id' => $content->subject_id, 'book_id' => $content->book_id, 'topic_id' => $content->topic_id]) }}">
+        data-row-data="{{ json_encode(['id' => $content->id, 'title' => $content->title, 'tags' => $content->tags, 'img_type' => $content->img_type, 'img_url' => $content->img, 'src_type' => $content->src_type, 'url' => $content->src, 'price' => $content->price, 'about' => $content->about, 'standard_id' => $content->standard_id, 'subject_id' => $content->subject_id, 'book_id' => $content->book_id, 'topic_id' => $content->topic_id, 'duration' => $content->duration]) }}">
         <svg class="icon icon-lg text-primary">
             <use xlink:href="{{ url('coreui/vendors/@coreui/icons/svg/free.svg#cil-pencil') }}">
             </use>
